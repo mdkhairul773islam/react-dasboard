ee975083710ed59a88df8510a47c027f3992c21e
Khairul Islam Tonmoy <mdkhairul773@gmail.com>
2021-09-20 15:39:06 +0600

    redux devtols set

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from '../redux/reducers/index'
 
 const initialSate = {}
 
+
 export const store = createStore(
     rootReducer,
-    initialSate
-)
+    initialSate,
+    composeWithDevTools(applyMiddleware(logger))
 
+)
