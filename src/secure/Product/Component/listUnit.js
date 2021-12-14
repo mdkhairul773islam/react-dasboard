4acf454af767a775efc7b532086d796415496f2d
mdkahirul773islam <mdkahirul773@gmail.com>
2021-12-15 00:32:10 +0600

    update check out

--- src/secure/Product/Component/listUnit.js
+++ src/secure/Product/Component/listUnit.js
@@ -1,14 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { DataService } from "../../../config/dataService/dataService";
 import { Form, Button, Modal } from "react-bootstrap";
 import KitchenSinkStory from "react-data-table-component";
 
+import { useForm } from "react-hook-form";
+
 function ListUnit(props) {
+
+  const [unit, setUnit] = useState([]);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
-  const handleShow = (e) => {
-    console.log(e.target.id);
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const handleShow = async (e) => {
     setShow(true);
+    try {
+      var id = e.target.id;
+      const res = await DataService.get(`/unit-edit/${id}`);
+      setUnit(res.data);
+
+    } catch (error) {
+      console.log("error");
+    }
+  }
+
+
+  const onSubmit = async (data, e) => {
+    const { id, unit } = data.unit;
+    try {
+      const res = await DataService.post("unit-update", { id: id, unit: unit });
+      console.log('res', res);
+      //setUnits(res.data);
+    } catch (error) {
+      console.log("error");
+    }
+
   };
+
+
+  useEffect(() => {
+    reset({
+      unit
+    })
+  }, [reset, unit]);
 
   const handleButtonClick = (e) => {
     console.log(e.target.id);
@@ -69,16 +104,29 @@ function ListUnit(props) {
           <Modal.Title as="h5">Edit Unit</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form autoComplete="off">
-            <Form.Control placeholder="Unit Name" />
+          <Form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+            <Form.Control
+              name="unit"
+              required
+              {...register("unit.unit", { required: true })}
+              placeholder="Unit Name"
+            />
+
+            <Form.Control
+              name="id"
+              required
+              {...register("unit.id", { required: true })}
+              placeholder="Unit Name"
+            />
+
           </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" size="sm" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="success" size="sm" onClick={handleClose}>
-            Update Changes
+          <Button variant="success" size="sm" onClick={handleSubmit(onSubmit)}>
+            Update
           </Button>
         </Modal.Footer>
       </Modal>
