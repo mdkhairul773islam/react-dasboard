2be44af45e2cc45fa4211d813051bd46334d6aa9
mdkahirul773islam <mdkahirul773@gmail.com>
2022-01-09 21:55:10 +0600

    category complete

--- src/secure/Product/category.js
+++ src/secure/Product/category.js
@@ -1,22 +1,160 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { useForm } from "react-hook-form";
 import AdminWraper from "../../components/layouts/AdminWraper";
 import Navbar from "../../secure/Product/navbar";
-import CategoryList from "../../secure/Product/categoryList";
+
+import KitchenSinkStory from "react-data-table-component";
+import { DataService } from "../../config/dataService/dataService";
+
+import { useToasts } from "react-toast-notifications";
+
 import {
     Container,
     Row,
     Col,
     Card,
     Form,
-    Button
-
+    Modal,
+    Button,
 } from "react-bootstrap";
 
 
 function Category(props) {
+    const [show, setShow] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [category, setCategory] = useState([]);
+
+    const { addToast } = useToasts();
+    const handleClose = () => setShow(false);
+
+    /* Category database store here */
+
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            name: "",
+        },
+    });
+    const onSubmit = async (data, e) => {
+        setLoading(true);
+        try {
+            const res = await DataService.post("category-store", data);
+            addToast("Saved Successfully", { appearance: "success" });
+            setCategory(res.data);
+            setLoading(false);
+        } catch (error) {
+            console.log("error");
+        }
+        e.target.reset();
+    };
+
+    useEffect(() => {
+
+        async function getCategory() {
+            setLoading(true);
+            try {
+                const res = await DataService.get("category");
+                setCategory(res.data);
+                setLoading(false);
+            } catch (error) {
+                console.log("error");
+            }
+        }
+        getCategory();
+    }, []);
+
+    /* Category database store end here */
+
+    /* Category update start here */
+    const { register: register2, handleSubmit: handleSubmit2, reset } = useForm();
+    const [editCategory, setEditCategory] = useState([]);
+
+    const handleShow = async (e) => {
+        setShow(true);
+        try {
+            var id = e.target.id;
+            const res = await DataService.get(`/category-edit/${id}`);
+            setEditCategory(res.data);
+        } catch (error) {
+            console.log("error");
+        }
+    };
+
+    const onSubmitUpdate = async (data, e) => {
+
+        const { id, name } = data.editCategory;
+
+        try {
+            setLoading(true);
+            const res = await DataService.post("category-update", { id: id, name: name });
+            addToast("Updated Successfully", { appearance: "info" });
+            setCategory(res.data);
+            setLoading(false);
+            setShow(false);
+        } catch (error) {
+            console.log("error");
+        }
+    };
+
+    /* Category delete  */
+    const handleDeleteClick = async (e) => {
+        try {
+            var id = e.target.id;
+            var confirmDelete = window.confirm("Want to delete?");
+            if (confirmDelete) {
+                const res = await DataService.get(`/category-destroy/${id}`);
+                addToast("Successfully Deleted", { appearance: "error" });
+                setCategory(res.data);
+            }
+        } catch (error) {
+            console.log("error");
+        }
+    };
+
+    useEffect(() => {
+        reset({
+            editCategory,
+        });
+    }, [editCategory, reset]);
+
+    const columns = [
+        {
+            name: "Sl",
+            selector: (row, index) => index + 1,
+            maxWidth: "20px",
+        },
+        {
+            name: "Name",
+            selector: (row) => row.name,
+            sortable: true,
+        },
+        {
+            name: "Action",
+            cell: (row) => (
+                <>
+                    <Button
+                        onClick={handleShow}
+                        id={row.id}
+                        className="btn btn-success btn-sm m-1"
+                    >
+                        <i className="fas fa-pen fa-sm"></i>
+                    </Button>
+                    <Button
+                        className="btn btn-danger btn-sm m-1"
+                        onClick={handleDeleteClick}
+                        id={row.id}
+                    >
+                        <i className="fas fa-trash fa-sm"></i>
+                    </Button>
+                </>
+            ),
+            center: true,
+            className: "action-width",
+        },
+    ];
 
     return (
-        <AdminWraper menuOpen='product'>
+        <AdminWraper menuOpen="product">
             <Container className="p-0" fluid>
                 <Row>
                     <div className="body_nav">
@@ -27,22 +165,17 @@ function Category(props) {
                     <Col sm={12} className="mb-4">
                         <Card>
                             <Card.Header as="h4" className="fw-bold">
-                                New Category
+                                Category
                             </Card.Header>
                             <Card.Body>
-                                <Form autoComplete="off">
+                                <Form
+                                    key={1}
+                                    autoComplete="off"
+                                    onSubmit={handleSubmit(onSubmit)}
+                                >
                                     <Row className="justify-content-md-start">
-                                        <Col className="mb-3"
-                                            sm={6}
-                                            md={6}
-                                            lg={3}
-                                            xl={3}
-                                            xxl={3}
-                                            xs={12}
-                                        >
-                                            <Form.Control placeholder="Category Name" />
-                                        </Col>
                                         <Col
+                                            className="mb-3"
                                             sm={6}
                                             md={6}
                                             lg={3}
@@ -50,6 +183,14 @@ function Category(props) {
                                             xxl={3}
                                             xs={12}
                                         >
+                                            <Form.Control
+                                                name="name"
+                                                required
+                                                {...register("name", { required: true })}
+                                                placeholder="Category Name"
+                                            />
+                                        </Col>
+                                        <Col sm={6} md={6} lg={3} xl={3} xxl={3} xs={12}>
                                             <Button variant="primary" type="submit">
                                                 Submit
                                             </Button>
@@ -67,7 +208,58 @@ function Category(props) {
                                 Category List
                             </Card.Header>
                             <Card.Body>
-                                <CategoryList />
+                                <KitchenSinkStory
+                                    columns={columns}
+                                    data={category}
+                                    fixedHeader
+                                    fixedHeaderScrollHeight="90vh"
+                                    highlightOnHover
+                                    pagination
+                                    pointerOnHover
+                                    responsive
+                                    striped
+                                    progressPending={loading}
+                                />
+
+                                <Modal show={show} onHide={handleClose}>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title as="h5">Edit Category</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        <Form
+                                            key={2}
+                                            autoComplete="off"
+                                            onSubmit={handleSubmit2(onSubmitUpdate)}
+                                        >
+                                            <Form.Control
+                                                name="name"
+                                                required
+                                                {...register2("editCategory.name", { required: true })}
+                                                placeholder="Category Name"
+                                            />
+
+                                            <Form.Control
+                                                type="hidden"
+                                                name="id"
+                                                required
+                                                {...register2("editCategory.id", { required: true })}
+                                                placeholder="Category Name"
+                                            />
+                                        </Form>
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button variant="secondary" size="sm" onClick={handleClose}>
+                                            Close
+                                        </Button>
+                                        <Button
+                                            variant="success"
+                                            size="sm"
+                                            onClick={handleSubmit2(onSubmitUpdate)}
+                                        >
+                                            Update
+                                        </Button>
+                                    </Modal.Footer>
+                                </Modal>
                             </Card.Body>
                             <Card.Footer className="text-muted">&nbsp;</Card.Footer>
                         </Card>
